d547d45e07f3bfe1d90582a70775fa71bde7580a
somsomdah <somdah98@gmail.com>
2022-01-19 03:01:50 +0900

    [ADD] UserController : add methods

--- src/main/java/com/dasom/gongtalk/controller/UserController.java
+++ src/main/java/com/dasom/gongtalk/controller/UserController.java
@@ -3,13 +3,19 @@ package com.dasom.gongtalk.controller;
 import com.dasom.gongtalk.domain.board.Board;
 import com.dasom.gongtalk.domain.user.User;
 import com.dasom.gongtalk.dto.UserCreateRequest;
+import com.dasom.gongtalk.dto.UserInfoResponse;
+import com.dasom.gongtalk.dto.UserLoginByDeviceRequest;
+import com.dasom.gongtalk.dto.UserLoginResponse;
+import com.dasom.gongtalk.repository.UserRepository;
+import com.dasom.gongtalk.security.DevicePrincipal;
 import com.dasom.gongtalk.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -17,16 +23,38 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
 
+    private final UserRepository userRepository;
     private final UserService userService;
 
-    @PostMapping("user")
-    public ResponseEntity<User> createUser(@RequestBody UserCreateRequest userCreateRequest){
-        User user = userService.saveUser(userCreateRequest.getDeviceNum());
-        return ResponseEntity.created(URI.create(Integer.toString(user.getId()))).body(user);
+    @GetMapping public ResponseEntity<List<UserInfoResponse>> getAllUsers(){
+        List<User> users = (List<User>) userRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(UserInfoResponse.fromEntities(users));
     }
 
+    @GetMapping("{id}")
+    public ResponseEntity<UserInfoResponse> getOneUser(@PathVariable Integer id){
+        User user = userRepository.findById(id).get();
+        return ResponseEntity.status(HttpStatus.OK).body(UserInfoResponse.fromEntity(user));
+
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@Valid @RequestBody UserCreateRequest request){
+        User user = userService.save(request.getDeviceNum());
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<UserLoginResponse> login(@RequestBody UserLoginByDeviceRequest request){
+
+        UserLoginResponse response = userService.login(request.getDeviceNum());
+        return ResponseEntity.ok().body(response);
+    }
+
+
     @GetMapping("boards")
-    public ResponseEntity<List<Board>> getBookmarkedBoards(@AuthenticationPrincipal User user){
+    public ResponseEntity<List<Board>> getBookmarkedBoards(@AuthenticationPrincipal DevicePrincipal devicePrincipal){
+        User user = userRepository.findById(devicePrincipal.getId()).get();
         List<Board> boards = userService.getBookmarkedBoards(user);
         return ResponseEntity.ok().body(boards);
     }
