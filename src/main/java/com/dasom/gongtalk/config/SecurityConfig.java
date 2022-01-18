d65eb084f92728384095224de086fb889d39e8e1
somsomdah <somdah98@gmail.com>
2022-01-19 03:01:08 +0900

    [ADD] SecurityConfig : configure method

--- src/main/java/com/dasom/gongtalk/config/SecurityConfig.java
+++ src/main/java/com/dasom/gongtalk/config/SecurityConfig.java
@@ -1,27 +1,44 @@
 package com.dasom.gongtalk.config;
 
 
+import com.dasom.gongtalk.repository.UserRepository;
+import com.dasom.gongtalk.security.CustomUserDetailsService;
 import com.dasom.gongtalk.security.TokenAuthenticationFilter;
-import com.dasom.gongtalk.service.UserService;
+import com.dasom.gongtalk.security.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserService userService;
-    private final TokenAuthenticationFilter tokenAuthenticationFilter;
+    private final CustomUserDetailsService userDetailsService;
+    private final TokenProvider tokenProvider;
+    private final UserRepository userRepository;
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
 
     }
 
-
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/user").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/user/login").permitAll()
+                .antMatchers("/swagger-ui").permitAll();
+    }
 }
