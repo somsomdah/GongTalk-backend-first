bfd2dfb7c190375984fc97187f644cd75bd2046c
somsomdah <somdah98@gmail.com>
2022-01-19 03:05:48 +0900

    [MODIFY] UserDetailsService -> CustomUserDetailsService

--- src/main/java/com/dasom/gongtalk/security/CustomUserDetailsService.java
+++ src/main/java/com/dasom/gongtalk/security/CustomUserDetailsService.java
@@ -1,47 +1,34 @@
 package com.dasom.gongtalk.security;
-import com.dasom.gongtalk.domain.board.Board;
-import com.dasom.gongtalk.domain.user.Setting;
 import com.dasom.gongtalk.domain.user.User;
 import com.dasom.gongtalk.repository.BookmarkRepository;
 import com.dasom.gongtalk.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.{UserDetailsService => SecurityUserDetailsService};
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
-public class UserDetailsService implements  {
+public class CustomUserDetailsService implements UserDetailsService {
 
-    private UserRepository userRepository;
-    private final BookmarkRepository bookmarkRepository;
-    private final Principal principal;
-
-    public User saveUser(String deviceNum){
-        User createdUser = new User();
-        createdUser.setDeviceNum(deviceNum);
-        createdUser.setUsername();
-        createdUser.setPassword();
-        createdUser.setSetting(new Setting());
-
-        return createdUser;
-    }
-
-
-
-    public List<Board> getBookmarkedBoards(User user) {
-        return bookmarkRepository.findBoardsByUser(user);
-
-    }
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return principal.buildCredentials(userRepository.findByUsername(username));
+        return DevicePrincipal.create(userRepository.findByUsername(username));
     }
+
+    public UserDetails loadUserById(Integer id) {
+        return DevicePrincipal.create(userRepository.findById(id).get());
+    }
+
+    public UserDetails loadUserByObject(User user) {
+        return DevicePrincipal.create(user);
+    }
+
+
 
 
 }
