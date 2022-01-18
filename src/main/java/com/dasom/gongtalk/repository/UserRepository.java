26c2dc4df1a30274f1b76f05f9343bd852b025a6
somsomdah <somdah98@gmail.com>
2022-01-19 03:09:15 +0900

    [ADD] UserRepository : findByDeviceNum

--- src/main/java/com/dasom/gongtalk/repository/UserRepository.java
+++ src/main/java/com/dasom/gongtalk/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.dasom.gongtalk.repository;
 
 import com.dasom.gongtalk.domain.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     User findByUsername(String username);
+    User findByDeviceNum(String deviceNum);
 }
