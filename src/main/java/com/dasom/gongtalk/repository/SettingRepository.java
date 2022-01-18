700a98217b2e9ef3807738de5e8131fd9732c833
somsomdah <somdah98@gmail.com>
2022-01-19 03:03:48 +0900

    [CREATE] SettingRepository

---
+++ src/main/java/com/dasom/gongtalk/repository/SettingRepository.java
@@ -0,0 +1,9 @@
+package com.dasom.gongtalk.repository;
+
+import com.dasom.gongtalk.domain.user.Setting;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SettingRepository extends CrudRepository<Setting, Integer>{
+}
