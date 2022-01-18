7b8d55746b531e419f4d85ef73639eef5dde2cdd
somsomdah <somdah98@gmail.com>
2022-01-19 03:08:43 +0900

    [REMOVE] Optimize imports

--- src/main/java/com/dasom/gongtalk/repository/BoardRepository.java
+++ src/main/java/com/dasom/gongtalk/repository/BoardRepository.java
@@ -4,8 +4,6 @@ import com.dasom.gongtalk.domain.board.Board;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface BoardRepository extends CrudRepository<Board, Integer> {
 }
