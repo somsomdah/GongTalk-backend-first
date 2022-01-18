ef53c436d2fd73b3274b6916c1edba804eafa51e
somsomdah <somdah98@gmail.com>
2022-01-19 03:08:15 +0900

    [MODIFY, CREATE] TokenProvidor : Modify method createToken, Create mehtod getAuthentication

--- src/main/java/com/dasom/gongtalk/security/TokenProvider.java
+++ src/main/java/com/dasom/gongtalk/security/TokenProvider.java
@@ -2,28 +2,32 @@ package com.dasom.gongtalk.security;
 
 import com.dasom.gongtalk.config.AppProperties;
 import com.dasom.gongtalk.domain.user.User;
-import com.dasom.gongtalk.domain.user.User;
 import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.Duration;
 import java.util.Date;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class TokenProvider {
 
     private final AppProperties appProperties;
+    private final CustomUserDetailsService userDetailsService;
+    private int tokenDurationMin = 30;
 
-
-    public TokenProvider(AppProperties appProperties) {
-        this.appProperties = appProperties;
-    }
 
     public String createToken(User user){
         Date now = new Date();
-        Date expirationTime =new Date(now.getTime()+ Duration.ofMinutes(30).toMillis());
+        Date expirationTime =new Date(now.getTime()+ Duration.ofMinutes(tokenDurationMin).toMillis());
+
         return Jwts.builder()
                 .setSubject(user.getId().toString())
                 .setIssuedAt(now)
@@ -32,6 +36,10 @@ public class TokenProvider {
                 .compact();
     }
 
+    public Authentication getAuthentication(String token){
+        UserDetails userDetails = userDetailsService.loadUserById(this.getUserIdFromToken(token));
+        return new DeviceNumAuthenticationToken(userDetails, userDetails.getAuthorities());
+    }
 
     public Integer getUserIdFromToken(String token){
         Claims claims = Jwts.parser()
@@ -43,8 +51,8 @@ public class TokenProvider {
 
     public boolean validateToken(String token){
         try{
-            Jwts.parser().setSigningKey(appProperties.getAuth().getTokenSecret()).parseClaimsJws(token);
-            return true;
+            Jws<Claims> claims = Jwts.parser().setSigningKey(appProperties.getAuth().getTokenSecret()).parseClaimsJws(token);
+            return !claims.getBody().getExpiration().before(new Date());
         }catch(Exception ex){
             System.out.println(ex.toString());
             log.error(ex.toString());
