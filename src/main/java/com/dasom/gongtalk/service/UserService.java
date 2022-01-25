9684b43d1b8a064f8843308e0c18be43e3f679d0
somsomdah <somdah98@gmail.com>
2022-01-25 15:04:50 +0900

    [Add] UserService : getFromPrincipal, getFromId

--- src/main/java/com/dasom/gongtalk/service/UserService.java
+++ src/main/java/com/dasom/gongtalk/service/UserService.java
@@ -4,15 +4,20 @@ import com.dasom.gongtalk.domain.board.Board;
 import com.dasom.gongtalk.domain.user.Setting;
 import com.dasom.gongtalk.domain.user.User;
 import com.dasom.gongtalk.dto.BoardInfoResponse;
+import com.dasom.gongtalk.dto.PostResponse;
 import com.dasom.gongtalk.dto.UserLoginResponse;
+import com.dasom.gongtalk.exception.ResourceNotFoundException;
+import com.dasom.gongtalk.repository.PostRepository;
 import com.dasom.gongtalk.repository.SettingRepository;
 import com.dasom.gongtalk.repository.SubscribeRepository;
 import com.dasom.gongtalk.repository.UserRepository;
+import com.dasom.gongtalk.security.DevicePrincipal;
 import com.dasom.gongtalk.security.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +26,21 @@ public class UserService {
     private final UserRepository userRepository;
     private final SettingRepository settingRepository;
     private final SubscribeRepository subscribeRepository;
+    private final PostRepository postRepository;
     private final TokenProvider tokenProvider;
+
+    public User getFromPrincipal(DevicePrincipal devicePrincipal){
+        return getFromId(devicePrincipal.getId());
+    }
+
+    public User getFromId(Integer id){
+
+        Optional<User> user = userRepository.findById(id);
+        if (user == null){
+            throw new ResourceNotFoundException("User", "id", id);
+        }
+        return user.get();
+    }
 
     public User save(String deviceNum){
         User newUser = new User(deviceNum);
@@ -36,7 +55,11 @@ public class UserService {
     }
 
     public List<BoardInfoResponse> getBoards(User user) {
-        return BoardInfoResponse.fromUser(user, subscribeRepository);
+        return BoardInfoResponse.fromUserEntity(user, subscribeRepository);
+    }
+
+    public List<PostResponse> getPosts(User user){
+        return PostResponse.fromUserEntity(user, postRepository);
     }
 
 
