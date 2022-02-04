471a95a064d3fec864ed730c90a1044f8f2f1085
somsomdah <somdah98@gmail.com>
2022-02-04 17:26:32 +0900

    [School] 제약조건 추가

--- src/main/java/com/dasom/gongtalk/domain/School.java
+++ src/main/java/com/dasom/gongtalk/domain/School.java
@@ -4,16 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name="school", uniqueConstraints = {@UniqueConstraint(columnNames = {"name", "campus"})})
 public class School {
 
     @Id
