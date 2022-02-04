166241c75423a86b566251c280673ac7fd72af71
somsomdah <somdah98@gmail.com>
2022-02-04 20:28:32 +0900

    [KeywordExtractor] 명사일 때에만 키워드 추출  + Refactor

--- src/main/java/com/dasom/gongtalk/util/KeywordExtractor.java
+++ src/main/java/com/dasom/gongtalk/util/KeywordExtractor.java
@@ -16,26 +16,25 @@ public class KeywordExtractor {
     public List<String> extract(String string){
 
         string = string.replaceAll("[^\uAC00-\uD7A30-9a-zA-Z]", " "); // 특수문자 제거
-
         KomoranResult result = komoran.analyze(string);
 
         List<String> keywords = new ArrayList<>();
         List<Token> tokenList = result.getTokenList();
 
-        for (Token token : tokenList){
+        for (Token token : tokenList) {
+            String pos = token.getPos();
             String keywordString = token.getMorph();
-            if (keywords.contains(keywordString)){
-                continue;
+            if (!keywords.contains(keywordString) && pos.startsWith("NN")) {
+                keywords.add(keywordString);
             }
-            keywords.add(keywordString);
         }
+
 
         String[] keywordStrings = string.split(" ");
         for (String keywordString : keywordStrings){
-            if (keywords.contains(keywordString)){
-                continue;
+            if (!keywords.contains(keywordString)){
+                keywords.add(keywordString);
             }
-            keywords.add(keywordString);
         }
 
         return keywords;
