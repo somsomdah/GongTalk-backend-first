f43b700067a220ef4839fa5e48ad04dfc234d6e9
somsomdah <somdah98@gmail.com>
2022-02-04 17:26:59 +0900

    [BaseException] Reformat

--- src/main/java/com/dasom/gongtalk/exception/BaseException.java
+++ src/main/java/com/dasom/gongtalk/exception/BaseException.java
@@ -4,6 +4,6 @@ public class BaseException extends RuntimeException{
 
     public BaseException(String message){
         super(message);
-        System.out.println(String.format("[Exception] %s", message));
+        System.out.printf("[Exception] %s%n", message);
     }
 }
