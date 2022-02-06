58a7c8f17758ba6c5caf8448cac30e23b505f174
somsomdah <somdah98@gmail.com>
2022-02-06 12:47:36 +0900

    [AppProperties] Data -> Getter

--- src/main/java/com/dasom/gongtalk/config/AppProperties.java
+++ src/main/java/com/dasom/gongtalk/config/AppProperties.java
@@ -1,23 +1,22 @@
 package com.dasom.gongtalk.config;
 
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@Data
+@Getter
 @EnableConfigurationProperties
 @ConfigurationProperties(prefix = "app")
 public class AppProperties {
 
     private final Auth auth = new Auth();
 
-    @Data
+    @Getter
     @NoArgsConstructor
     public static class Auth{
         private String tokenSecret;
         private int tokenDurationMin;
     }
-
 
 }
