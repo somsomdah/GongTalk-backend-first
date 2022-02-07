a904f2e3ba291121f2abff9250a2b75727042e7d
somsomdah <somdah98@gmail.com>
2022-02-07 22:09:27 +0900

    [RefreshTokenProvider] Create

---
+++ src/main/java/com/dasom/gongtalk/security/RefreshTokenProvider.java
@@ -0,0 +1,25 @@
+package com.dasom.gongtalk.security;
+
+import com.dasom.gongtalk.config.AppProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RefreshTokenProvider extends TokenProvider {
+
+
+    public RefreshTokenProvider(AppProperties appProperties, CustomUserDetailsService userDetailsService, AppProperties appProperties1){
+        super(appProperties, userDetailsService);
+    }
+
+    @Override
+    void setTokenDurationMin(){
+        this.tokenDurationMin = this.appProperties.getAuth().getRefreshTokenDurationMin();
+    }
+
+    @Override
+    void setTokenSecret(){
+        this.tokenSecret = this.appProperties.getAuth().getRefreshTokenSecret();
+    }
+
+
+}
