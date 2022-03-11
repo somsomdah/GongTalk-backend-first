2e42a314f0954073da4f8e21faa754b72d354a4f
somsomdah <somdah98@gmail.com>
2022-03-11 15:26:44 +0900

    공지사항 클래스 추가

---
+++ src/main/java/com/dasom/gongtalk/domain/Notice.java
@@ -0,0 +1,37 @@
+package com.dasom.gongtalk.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="notice")
+public class Notice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private LocalDate date;
+
+    @NotNull
+    @Column(unique = true)
+    private String url;
+
+    public Notice(String title, String url){
+        this.title = title;
+        this.url = url;
+        this.date = LocalDate.now();
+    }
+}
