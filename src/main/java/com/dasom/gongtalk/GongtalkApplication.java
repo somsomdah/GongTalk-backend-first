c6a2c7476f67b10c60b1a7b94bcdf019de12bac8
somsomdah <somdah98@gmail.com>
2022-03-14 17:50:11 +0900

    Timezone 설정

--- src/main/java/com/dasom/gongtalk/GongtalkApplication.java
+++ src/main/java/com/dasom/gongtalk/GongtalkApplication.java
@@ -6,10 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableConfigurationProperties(value = {AppProperties.class})
 @EnableScheduling
 @SpringBootApplication
 public class GongtalkApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GongtalkApplication.class, args);
