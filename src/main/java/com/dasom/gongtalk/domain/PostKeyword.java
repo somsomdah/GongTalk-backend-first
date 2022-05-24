62fcb55c9c783ffc9018845a4246d305fd28a92c
somsomdah <somdah98@gmail.com>
2022-05-24 19:23:30 +0900

    PK 생성규칙 명시

--- src/main/java/com/dasom/gongtalk/domain/PostKeyword.java
+++ src/main/java/com/dasom/gongtalk/domain/PostKeyword.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class PostKeyword {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @ManyToOne(fetch = FetchType.EAGER)
