79950189ec10d564201f14f1a0da2b4a86848f69
somsomdah <somdah98@gmail.com>
2022-06-14 18:44:41 +0900

    필드명 변경

--- src/main/java/com/dasom/gongtalk/domain/UserBoard.java
+++ src/main/java/com/dasom/gongtalk/domain/UserBoard.java
@@ -7,9 +7,9 @@ import lombok.RequiredArgsConstructor;
 import javax.persistence.*;
 
 @Entity
+@Data
 @AllArgsConstructor
 @RequiredArgsConstructor
-@Data
 @Table(name="user_board", uniqueConstraints = @UniqueConstraint(columnNames = {"user_id","board_id"}))
 public class UserBoard {
 
@@ -24,7 +24,7 @@ public class UserBoard {
     private Board board;
 
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Integer order;
+    private Integer orderValue; // NOTE: order는 예약어이기 때문에 이를 컬럼명으로 하면 안됨!
 
     public UserBoard(User user, Board board){
         this.user = user;
