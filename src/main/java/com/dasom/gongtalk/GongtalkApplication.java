7aa5a76ba6b907d0acdd276ce04d0e996215995b
somsomdah <somdah98@gmail.com>
2022-07-03 21:12:28 +0900

    자동배포 TEST

--- src/main/java/com/dasom/gongtalk/GongtalkApplication.java
+++ src/main/java/com/dasom/gongtalk/GongtalkApplication.java
@@ -20,6 +20,7 @@ public class GongtalkApplication {
 	}
 
 	public static void main(String[] args) {
+		System.out.println("========================== 자동배포 Test ==========================");
 		SpringApplication.run(GongtalkApplication.class, args);
 	}
 
